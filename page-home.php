18978c4402ee1ebc73ccf001dcc7bb24e768efc2
Altab Hossain <altab_ru@yahoo.com>
2018-04-20 18:30:49 -0400

    Home page with _homepage.scss

---
+++ page-home.php
@@ -0,0 +1,22 @@
+
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="item container-item-1">
+    <div class="image-1">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/placeimg_640_640_arch.jpg" alt="Image-1">
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <button class="button" type="button" name="button">Button 1</button>
+  </div>
+  <div class="vertical-line">
+    <div class="line"></div>
+  </div>
+  <div class="item container-item-2">
+    <div class="image-2">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/placeimg_640_640_nature.jpg" alt="Image-2">
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <button class="button" type="button" name="button">Button 2</button>
+  </div>
+</div>
