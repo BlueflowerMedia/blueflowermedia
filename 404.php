6846cc702c7608b4d470e9cdf0f63e7b0178a2e8
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-04-26 14:07:40 -0400

    funny 404 changes again

--- 404.php
+++ 404.php
@@ -19,7 +19,7 @@ get_header();
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'blueflowermedia' ); ?></p>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. What have you done?!?!? Maybe try one of the links below or a search?', 'blueflowermedia' ); ?></p>
 
 					<?php
 					get_search_form();
