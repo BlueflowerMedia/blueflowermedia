78c3b93ee7458af135d14cac8714ec794c0334ba
Altab Hossain <altab_ru@yahoo.com>
2018-05-07 16:17:43 -0400

    Editing single page

--- single.php
+++ single.php
@@ -10,28 +10,54 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
+<section id="blogpost">
+	<div class="container-blog">
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
+			//the_post_navigation();
 
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// Store the post category
+			$post_cat_id = get_the_category()[0]->term_id;
 
 		endwhile; // End of the loop.
 		?>
+	</div>
+</section>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<!-- Related posts -->
+<section id="related-posts">
+	<div class="container-blog">
+		<h1>Related Posts</h1>
+		<?php
+		$args = array(
+			'cat' => $post_cat_id,
+			'posts_per_page' => 3
+		);
+
+		$query = new WP_Query($args);
+
+		if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+			<div class="related-post">
+				<div class="related-post-thumb">
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+				</div>
+				<div class="related-post-content">
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				</div>
+			</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</section>
+<!-- Related posts -->
+
+
+
+
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
