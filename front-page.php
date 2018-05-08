41067398b740c11e194f62d7fe58881540a78b9d
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-08 11:29:58 -0400

    add Noman text to front page

--- front-page.php
+++ front-page.php
@@ -5,6 +5,9 @@
   <div id="particles"></div>
   <div class="container">
     <h1 class="sr blue">Make Your Business Bloom</h1>
+    <h3>We help small businesses grow digitally.</h3>
+    <h3>The digital world is changing at an ever growing rate,</h3>
+    <h3>our job is to make sure your business adapts to market demands.</h3>
 
   </div>
 </section>
@@ -14,8 +17,8 @@
   <div class="container flex-row">
     <div class="left">
       <h1 class="white">About Us<span class="big-dot yellow">.</span></h1>
-      <p class="white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente error ipsam quis, facilis explicabo laudantium asperiores officiis in debitis! Natus nostrum pariatur sint quis, blanditiis, temporibus. We are a Toronto
-        Development & Marketing Business helping small businessess from all industries.</p>
+      <p class="white">We are a Toronto based digital agency specializing in creating, improving, and managing your digital footprints. We work with clients from the ideation phase - brainstorming branding concepts, to developing and launching projects for mass market
+        use. With an emphasis on user experience, we aim to make your website/app as intuitive and pleasant as possible. As we do all of our work in house, our people are the core of our company.</p>
     </div>
     <div class="fbox" id="fbox1" style="transform: rotate(45deg);" data-rellax-speed="3"></div>
     <div class="fbox" id="fbox2" style="transform: rotate(45deg);" data-rellax-speed="5"></div>
@@ -27,7 +30,35 @@
 <section id="section-services">
   <div class="container">
     <h1>Services<span class="big-dot">.</span></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente error ipsam quis, facilis explicabo laudantium </p>
+    <div class="root flex-row">
+      <div class="subservice wrap">
+        <p>Our in house services are flexible and customized based on our clients needs. Here are our general services we provide, talk to us to create a customized plan to help you achieve your specific goals!</p>
+      </div>
+      <div>
+        <h3>Web Design:</h3>
+        <p>We build results oriented intuitive websites that leave a lasting impression.</p>
+      </div>
+      <div>
+        <h3>Graphic Design:</h3>
+        <p>“Good design is good business” - Thomas J. Watson Jr.</p>
+      </div>
+      <div>
+        <h3>Digital Marketing</h3>
+        <p>“The only thing left to do after you build your business, is to grow your business!” - BlueFlower Media</p>
+      </div>
+      <div>
+        <h3>Social Media Management:</h3>
+        <p>We manage social profiles across all platforms. Engage with your audience real time and effectively.</p>
+      </div>
+      <div>
+        <h3>Media Management:</h3>
+        <p>Grow your media business with newspaper/magazine design, advertiser management services, and more!</p>
+      </div>
+      <div>
+        <h3>Cloud Hosting:</h3>
+        <p>Host your website/application on our secure platform. Affordable & Reliable.</p>
+      </div>
+    </div>
   </div>
 </section>
 
@@ -36,9 +67,9 @@
   <div class="container">
     <h2>Split Contacts<span class="big-dot">.</span></h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente error ipsam quis, facilis explicabo laudantium asperiores officiis in debitis! Natus nostrum pariatur sint quis, blanditiis, temporibus. We are a Toronto
-      Development & Marketing Business helping small businessess from all industries.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente error ipsam quis, facilis explicabo laudantium asperiores officiis in debitis! Natus nostrum pariatur sint quis, blanditiis, temporibus. We are a Toronto
-      Development & Marketing Business helping small businessess from all industries.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente error ipsam quis, facilis explicabo laudantium asperiores officiis in debitis! Natus nostrum pariatur sint quis, blanditiis, temporibus. We are a Toronto
-      Development & Marketing Business helping small businessess from all industries.</p>
+      Development & Marketing Business helping small businessess from all industries.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente error ipsam quis, facilis explicabo laudantium asperiores officiis in debitis!
+      Natus nostrum pariatur sint quis, blanditiis, temporibus. We are a Toronto Development & Marketing Business helping small businessess from all industries.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil unde, sunt nesciunt sapiente
+      error ipsam quis, facilis explicabo laudantium asperiores officiis in debitis! Natus nostrum pariatur sint quis, blanditiis, temporibus. We are a Toronto Development & Marketing Business helping small businessess from all industries.</p>
   </div>
 </section>
 
@@ -50,9 +81,18 @@
       <p>They all got a happy ending</p>
     </div>
     <div class="row owl-carousel client-logo-slider">
-      <div><img src="https://placeimg.com/300/200/any" alt=""></div>
-      <div><img src="https://placeimg.com/300/200/any" alt=""></div>
-      <div><img src="https://placeimg.com/300/200/any" alt=""></div>
+      <div>
+        <h3>India Journal</h3>
+        <img src="https://placeimg.com/300/200/any" alt="">
+      </div>
+      <div>
+        <h3>Mint One Financial</h3>
+        <img src="https://placeimg.com/300/200/any" alt="">
+      </div>
+      <div>
+        <h3>Outlier Recruitment</h3>
+        <img src="https://placeimg.com/300/200/any" alt="">
+      </div>
       <div><img src="https://placeimg.com/300/200/any" alt=""></div>
     </div>
   </div>
@@ -71,14 +111,31 @@
           <div class="logo">
             <img src="https://placeimg.com/100/100/arch" alt="">
           </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur error neque adipisci architecto fuga voluptas optio </p>
+          <p>Blueflower Media created my e-commerce website for my artwork from scratch. In a short period of time the website was up and working with great visuals and smooth functionality for visitors. Staff are friendly, flexible and the project was completed
+            on budget. I recommend them :)</p>
         </div>
-        <div class="t-bot">
-          <h3>Name</h3>
+        <div class="t-bot flex-row">
+          <h3>Christian Turner,</h3>
+          <h4>Owner CT Paintings</h4>
         </div>
       </div>
       <!--   Testimonia 1 end -->
-      <!--   Testimonial 1 begin -->
+      <!--   Testimonial 2 begin -->
+      <div class="test">
+        <div class="t-top flex-row">
+          <div class="logo">
+            <img src="https://placeimg.com/100/100/arch" alt="">
+          </div>
+          <p>BlueFlower went above and beyond the project scope, making sure we were totally satisfied with the project.</p>
+        </div>
+        <div class="t-bot flex-row">
+          <h3>Mike Ross, President & CEO</h3>
+          <h4>XYZ Media Corp.</h4>
+
+        </div>
+      </div>
+      <!--   Testimonia 2 end -->
+      <!--   Testimonial 3 begin -->
       <div class="test">
         <div class="t-top flex-row">
           <div class="logo">
@@ -86,24 +143,11 @@
           </div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur error neque adipisci architecto fuga voluptas optio </p>
         </div>
-        <div class="t-bot">
+        <div class="t-bot flex-row">
           <h3>Name</h3>
         </div>
       </div>
-      <!--   Testimonia 1 end -->
-      <!--   Testimonial 1 begin -->
-      <div class="test">
-        <div class="t-top flex-row">
-          <div class="logo">
-            <img src="https://placeimg.com/100/100/arch" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur error neque adipisci architecto fuga voluptas optio </p>
-        </div>
-        <div class="t-bot">
-          <h3>Name</h3>
-        </div>
-      </div>
-      <!--   Testimonia 1 end -->
+      <!--   Testimonia 3 end -->
     </div>
   </div>
 </section>
