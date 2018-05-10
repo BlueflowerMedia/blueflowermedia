c853a1508a76e8c24fb77891d98f6f7b8eee4a2a
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-10 17:34:49 -0400

    service part

--- page-team.php
+++ page-team.php
@@ -31,13 +31,13 @@
         <div class="member sr">
           <img id="pic1" src="<?php echo get_template_directory_uri(); ?>/images/team-noman-1.jpg" alt="Noman Hossain">
           <h3>Noman Hossain</h3>
-          <p>&#60; Lead Designer . Project Manager &#62;</p>
+          <p>[ Lead Designer<br>Project Manager ]</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </div>
         <div class="member sr">
           <img id="pic2" src="<?php echo get_template_directory_uri(); ?>/images/team-fahim-1.jpg" alt="Fahim Shahreer">
           <h3>Fahim Shahreer</h3>
-          <p>{{ Lead Developer . Project Manager }}</p>
+          <p>[ Lead Developer<br>Project Manager ]</p>
           <p>Office never has enough coffee for me.</p>
         </div>
       </div>
@@ -46,19 +46,19 @@
         <div class="member sr">
           <img id="pic3" src="<?php echo get_template_directory_uri(); ?>/images/team-altab-1.jpg" alt="Altab Hossain">
           <h3>Altab Hossain</h3>
-          <p><span class="red">?php</span> <span class="blue">$role</span> = Backend Developer; <span class="red">?></span></p>
+          <p>[ Backend Developer ]</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </div>
         <div class="member sr">
           <img id="pic4" src="<?php echo get_template_directory_uri(); ?>/images/team-ali-1.jpg" alt="Ali Akbar Sarkarati">
           <h3>Ali Akbar Sarkarati</h3>
-          <p>{Front-end Developer}</p>
+          <p>[ Front-end Developer ]</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </div>
         <div class="member sr">
           <img id="pic5" src="<?php echo get_template_directory_uri(); ?>/images/team-zulfiqur-1.jpg" alt="Zulfiqur Rahman">
           <h3>Zulfiqur Rahman</h3>
-          <p>[Junior Developer]</p>
+          <p>[ Junior Developer ]</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         </div>
       </div>
