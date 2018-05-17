44e405d1ef0478f702e18c3d93f6e082bb136b02
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-17 17:32:21 -0400

    add ali

--- page-team.php
+++ page-team.php
@@ -54,7 +54,7 @@
           <p>I am always curious about hardest problems in the office.</p>
         </div>
         <div class="member sr">
-          <img id="pic4" src="<?php echo get_template_directory_uri(); ?>/images/team/team-ali-1.jpg" alt="Ali Akbar Sarkarati">
+          <img id="pic4" src="<?php echo get_template_directory_uri(); ?>/images/team/ali-akbar-headshot.jpg" alt="Ali Akbar Sarkarati">
           <h3>Ali Akbar Sarkarati</h3>
           <p>[ Front-end Developer ]</p>
           <p>Technology is my passion.</p>
