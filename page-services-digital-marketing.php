d02a50a42009376b0fd23bf890e1a8baf0f6ef35
Noman Hossain <noman@blueflowermedia.com>
2018-06-07 23:15:09 -0400

    digital marketing content

--- page-services-digital-marketing.php
+++ page-services-digital-marketing.php
@@ -7,13 +7,16 @@
     </div>
     <div class="txtservice flex2">
       <h1>Digital Marketing<span class="big-dot blue">.</span></h1>
-      <p>We help media and publishing companies with print design and resource management for online advertising.</p>
+      <p>In a multi-device world, its very important to attract and retain users on as many touchpoints as possible. For us, the first step is to have a great presence on as many relavant platforms as possible in a practical integrated way.
+      The next step is find appropriate advertising opportunities that will attract the right customers to your platforms/landing pages. The need for amazing user experience will be very objous here in this stage of your digital marketing process, because
+    great design just converts so much more than bland design! We then work towards improving your conversion rate & your customer return rate.  </p>
     </div>
   </div>
 </section>
 <section id="rd">
   <div class="container">
-    <p class="white">Digital Marketing consists of a wide array of services and need to catered to specific business needs. Our digital marketing hourly rate is $60/hour. Pleasecontact us with your specific project details so we can give you a precise quote.</p>
+    <p class="white">Digital Marketing consists of a wide array of services and needs to be catered to your specific business needs. We do not currently have any digital marketing packages. Please contact
+    us with your project details so we can assist you further.</p>
   </div>
 </section>
 <!--  Digital Marketing design ------------------------------------------------------------------------------>
