4e5649427b791c0ed2593d3ca317066bfc44b13b
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-08-30 13:44:38 -0400

    10px margin added

--- header.php
+++ header.php
@@ -78,7 +78,7 @@ $cloudhosting = get_permalink(get_page_by_path( 'services-cloud-hosting' ));
 						'container_class' => 'mobile-menu-list',
 					) );
 				?>
-					<a href="<?php echo $contact; ?>"><button class="nav-contact" type="button" name="button">Contact Us</button></a>
+					<a id="10pxgap" href="<?php echo $contact; ?>"><button class="nav-contact" type="button" name="button">Contact Us</button></a>
 				<h2>Our Services<span class="big-dot blue">.</span></h2>
 				<?php
 					wp_nav_menu( array(
