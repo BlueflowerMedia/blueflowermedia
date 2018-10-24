54a5a24b0ef2056bd815ac22c9a6ced4dd0e6dd9
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-10-24 14:53:20 -0400

    Digital Packages Page added.

--- page-web-design.php
+++ page-web-design.php
@@ -31,21 +31,6 @@
 
 
 
-<!-- section image and text side by side-->
-<!-- <section id="section-header-2">
-  <div class="image-text-2">
-    <div class="banner-text-2">
-      <h2>It's  your  website.  It's  your  business,  and  it's  your  clients.</h2><br>
-      <h5>So  it  only  makes  sense  that  our  web  design  process  starts  with  You.</h5>
-      <p>At  BlueFlower  Media,  we  consider  your  content  structure,  user-pathways  and  methods  to  increase  visitor  retention  rate  from  the  very  start.  We  keep  you
-          updated  at  every  step  of  the  process  and  consider  your  feedback  during  our  development,  so  that  the  final  product  is  always  better  than  what  you</p>
-    </div>
-    <div class="banner-image-2">
-
-    </div>
-  </div>
-</section> -->
-
 <!-- section web-design-process -->
 <section id="web-design-process">
 <div class="process-container">
@@ -66,7 +51,7 @@
       </div>
       <div class="figure">
         <div class="circle">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/ui-ux-research-and-design-icon.svg" alt="" class="image">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/UI&UX.svg" alt="" class="image">
         </div>
         <div class="text">
           <p><strong>UI/UX  Research<br>and  Design</strong></p>
@@ -79,7 +64,7 @@
 
       <div class="figure">
         <div class="circle">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/coding-and-development-icon.svg" alt="" class="image">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/Coding&Dev.svg" alt="" class="image">
         </div>
         <div class="text">
           <p><strong>Coding  and  Development</strong></p>
@@ -92,7 +77,7 @@
 
       <div class="figure">
         <div class="circle">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/launch-icon.svg" alt="" class="image">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/Launch.svg" alt="" class="image">
         </div>
         <div class="text">
           <p><strong>Launch</strong><br><br></p>
@@ -106,7 +91,7 @@
 
       <div class="figure">
         <div class="circle">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/web-maintenance-icon.svg" alt="" class="image">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/Maintenance&Opt.svg" alt="" class="image">
         </div>
         <div class="text">
           <p><strong>Maintenance  and  Optimization</strong></p>
@@ -148,28 +133,6 @@
 
 </section>
 
-<!-- <section id="section-header-3">
-  <div class="image-text-3">
-    <div class="banner-image-3">
-
-    </div>
-    <div class="banner-text-3">
-      <h2>Build  your  Dream  E-Commerce  Platform.</h2><br>
-      <p><strong>So  it  only  makes  sense  that  our  web  design  process  starts  with  You.</strong></p>
-      <p>Convert  your  visitors  into  buyers  with  successful  e-commerce  business  strat-egies  that  involve  innovative  marketing  and  a  robust  online  platform.
-         Our  team  of  designers  and  developers  work  in  synergy  to  create  softwares  that  not  only  look  amazing,  but  also  utilize  proven  user-interaction
-         ideologies  to  increase  sales.  With  our  result-driven  strategies,  growth  is  inevitable.  So  with  BlueFlower  Media,  grow  your  commerce  with  reassurance
-         as  we  build  our  platforms  with  scalability  in  mind  and  integrate  industry-standard  automa-tion  channels  for  product  management.</p>
-      <p><strong>Grow Smart.</strong></p>
-      <p>Growth  in  retail  doesn't  come  easy.  It  takes  significant  data  on  product  per-formance  indicators  and  require  regular  optimization  to  build  a  successful
-         e-commerce  business.  We  integrate  Retail  Performance  Analytics  tools  that  help  you  make  the  right  decision  at  every  step  of  growing  your  business.  </p>
-      <p><strong>So  how  do  I  brand  and  market  my  e-commerce?</strong></p>
-      <p>Our  team  of  marketers  and  designers  have  been  using  result-driven  branding  and  industry-proven  marketing  strategies  to  create  successful  e-commerce  platforms.
-        Visit  our  marketing  and  digital  branding  pages  for  more  technical  information  about  our  methods.  </p>
-
-    </div>
-  </div>
-</section> -->
 
 <!-- section banner2 -->
 <section id="banner2-webdesign">
@@ -198,7 +161,7 @@
         <img src="<?php echo get_template_directory_uri(); ?>/images/webdesign/Server.svg" alt="" class="image">
   </div>
     <div class="hosting-text">
-      <h2>Hosting  And  Maintenance  Without  Any  Fuss.</h2>
+      <h2>Hosting & Maintenance<br>Without  Any  Fuss.</h2>
       <p>Why  do  you  have  to  pay  extra  fees  for  backup,  security  and  SSL?  Well,  you  don't!</p>
       <p>All  of  our  hosting  services  include  premium  features  such  as  customizable  backups,  firewall  and  SSL  certificates.  We  boast  an  uptime  of  99.9%  in  both  shared  and  dedicated  hosting  plans.  Our  sysadmins  are  always  prepared  to  setup  the  most  up-to-date  platforms  to  accommodate  for  websites  and  web  applications  that  you  are  building  or  transferring.</p>
         <p>If  you  want  to  be  a  leader  in  your  business,  it  is  crucial  that  your  website  is  updated  and  maintained  regularly.  At  BlueFlower  Media,  we  like  to  build  long  lasting  relationships  with  our  clients  by  providing  reliable  maintenance  services  so  that  you  don't  have  to  worry  about  it.  Some  of  these  services  in-clude  software  updates,  e-commerce  updates  and  content  management.</p>
