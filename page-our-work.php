863c7f9e725cb8f9cc9d47507e25728525824bbf
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-06 17:50:05 -0500

    get in touch section added in portfolio section

--- page-our-work.php
+++ page-our-work.php
@@ -58,12 +58,7 @@
 </section>
 
 <!-- section-getintouch-->
-<section id="section-getintouch">
-  <div class="container">
-    <a href="<?php echo get_permalink(get_page_by_path( 'contact' )); ?>"><button type="button" name="button">Let's Get Started!</button></a>
-  </div>
-</section>
-
+<?php get_template_part('template-parts/getintouch'); ?>
 
 
 
