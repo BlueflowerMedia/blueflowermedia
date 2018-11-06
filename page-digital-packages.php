70b81298936ea8e8d42511e9175d13d36db46833
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-06 17:13:37 -0500

    some more changes

--- page-digital-packages.php
+++ page-digital-packages.php
@@ -21,6 +21,7 @@
       <p><span class="yellow-background">Most Popular</span></p>
       <h3>New Business Package</h3>
       <p>Lorem ipsum dolor sit amet,Nunc Phasellus lobortis augue libero, id euismod enim pellentesque et. Nunc nec arcu vitae tellus lobortis hendrerit. Sed luctus purus turpis, et porttitor lacus accumsan id.</p>
+      <img class="shown-in-mobile-only" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset11.svg" alt="" class="image">
 
       <div class="accordion1">
         <button class="accordion">LOGO DESIGN</button>
@@ -63,7 +64,7 @@
       <p style="text-align:right"><span class="yellow-background">$1400.00</span></p>
     </div>
     <div class="package-right flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset11.svg" alt="" class="image">
+      <img  src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset11.svg" alt="" class="image">
 
     </div>
 
@@ -75,7 +76,7 @@
     <!-- <h1 class="">Packages<span class="big-dot yellow">.</span></h1> -->
     <div class="services flex-row">
 
-
+      <!-- service 1 -->
       <div class="package">
         <a class="service-anchor" href="<?php echo $webdesign; ?>">
           <div class="logo">
@@ -154,10 +155,92 @@
           </ul>
         </div>
       </div>
+
+      <!-- service 4 --->
+      <div class="package">
+        <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
+          <div class="logo">
+            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset9.svg" alt="bfm-digital-marketing-icon">
+            <h3>Brand Style Guide</h3>
+          </div>
+        </a>
+        <div class="package-content">
+          <p>$2,800.00</p>
+          <ul class="package-list">
+            <li>1 hour Website Consultation</li>
+            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
+            <li>Custom Wordpress Theme</li>
+            <li>Responsive and Cross Browser Tested</li>
+            <li>Google Console Setup + Google Analytics Setup</li>
+            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
+            <li>1 Hour Website Coaching</li>
+            <li>Social Media Integration</li>
+            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
+            <li>5 Website Pages</li>
+            <li>3 Sections Per Page</li>
+            <li>5 Custom Icons</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- service 4 --->
+      <div class="package">
+        <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
+          <div class="logo">
+            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset9.svg" alt="bfm-digital-marketing-icon">
+            <h3>Website Maintenance</h3>
+          </div>
+        </a>
+        <div class="package-content">
+          <p>$2,800.00</p>
+          <ul class="package-list">
+            <li>1 hour Website Consultation</li>
+            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
+            <li>Custom Wordpress Theme</li>
+            <li>Responsive and Cross Browser Tested</li>
+            <li>Google Console Setup + Google Analytics Setup</li>
+            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
+            <li>1 Hour Website Coaching</li>
+            <li>Social Media Integration</li>
+            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
+            <li>5 Website Pages</li>
+            <li>3 Sections Per Page</li>
+            <li>5 Custom Icons</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- service 4 --->
+      <div class="package">
+        <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
+          <div class="logo">
+            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset9.svg" alt="bfm-digital-marketing-icon">
+            <h3>Logo Design</h3>
+          </div>
+        </a>
+        <div class="package-content">
+          <p>$2,800.00</p>
+          <ul class="package-list">
+            <li>1 hour Website Consultation</li>
+            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
+            <li>Custom Wordpress Theme</li>
+            <li>Responsive and Cross Browser Tested</li>
+            <li>Google Console Setup + Google Analytics Setup</li>
+            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
+            <li>1 Hour Website Coaching</li>
+            <li>Social Media Integration</li>
+            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
+            <li>5 Website Pages</li>
+            <li>3 Sections Per Page</li>
+            <li>5 Custom Icons</li>
+          </ul>
+        </div>
+      </div>
     </div>
     <!-- service names end --->
   </div>
 </section>
+<?php get_template_part('template-parts/getintouch'); ?>
 
 
 <?php get_footer(); ?>
