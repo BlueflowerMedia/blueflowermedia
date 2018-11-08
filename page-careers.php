93077052b518e9521f968f2937a4c9cd55d601ba
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-08 16:20:44 -0500

    contact form added in careers page

--- page-careers.php
+++ page-careers.php
@@ -1,6 +1,53 @@
 <?php get_header(); ?>
 
 
+<section id="careers">
+  <div class="container">
+    <div class="txtservice flex1">
+      <h3>Blueflower Media Corp. Careers<span class="big-dot blue">.</span></h3>
+    </div>
+
+    <h2>Lead Developer</h2>
+    <h3>Location: Remote/Phillipines</h3>
+    <p>Thank you for considering a career at BlueFlower Media Corp., Please take a minute to fill out the following form. After you have completed your application an email will be sent to you with information about how to check the status of your application. </p>
+    <h4>About Us</h4>
+    <p>We’re entrepreneurs, both dynamic and creative. We understand what it takes to manage digital relationships between brands and consumers. Through agility in strategy and execution, we build journeys with measurable impact. Our company’s mantra is Damn Good Digital – signifying the leading edge solutions we offer to clients, including Honda Canada, Acura, Freedom Mobile, and Brookfield Homes. We pride ourselves on our entrepreneurial spirit and corporate culture. In fact, we recently took home fifth place at the 2017 Great Places to Work Canada awards ceremony.</p>
+    <h4>The Position</h4>
+    <p>As a Senior Front-End Developer, you will be reporting to the Director of Technology as well as the .NET Software Architect/Team Lead. All of our developers pride themselves in creating and building beautiful software that follows development best practices. Iterative and test driven, we follow an agile sprint-based methodology to produce products we are proud of. Our team builds an infrastructure that pushes performance boundaries.</p>
+    <h4>Responsibilities</h4>
+    <p>-Design, develop, and implement Front-End JavaScript applications<br>
+      -Plan, design, build, support, and maintain new and existing applications<br>
+      -Work closely with a team of developers, communicating effectively and working together to reach defined goals and meet deadlines<br>
+      -Attend and participate in daily standups and project retrospectives<br>
+      -Commit to ongoing learning and development<br>
+      -Ensure front-end development integrates with back-end development<br>
+      -Communicate clearly and effectively across all levels of the organization<br>
+    </p>
+
+    <h4>Requirements</h4>
+    <p>-5+ years of experience with JavaScript<br>
+      -jQuery<br>
+      -HTML5, CSS3<br>
+      -React and/or Angular<br>
+      -Git<br>
+      -Active participant in the development community
+    </p>
+
+    <h4>Salary</h4>
+    <p>26000 PHP/Month to 37000 PHP/Month</p>
+
+    <h4>Application Form</h4>
+    <?php echo do_shortcode('[contact-form-7 id="96" title="careers"]') ?>
+
+
+    <hr>
+    <p>We will follow up with potential candidates via email, and schedule a video interview thereafter.<br>-Thanks </p>
+
+  </div>
+
+</section>
+
+
 
 
 
