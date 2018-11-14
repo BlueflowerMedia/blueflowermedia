31ea49dc2e11d1e7b02355110e22df443d70c3a1
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-14 14:30:08 -0500

    some changes

--- page-contact.php
+++ page-contact.php
@@ -8,7 +8,7 @@
   <div class="container">
 
     <div class="row flex-row">
-      <div class="contact-type flex1">
+      <!-- <div class="contact-type flex1">
         <i class="fas fa-mobile-alt"></i>
         <h4>Phone Number</h4>
         <p>647-712-6119</p>
@@ -22,6 +22,46 @@
         <h4>Hours</h4>
         <p>10 AM to 6 PM</p>
 
+      </div> -->
+
+      <div class="footer-right flex1">
+
+        <div class="contact-items">
+          <div class="contact">
+            <div class="contact-left">
+              <i class="fas fa-mobile-alt"></i>
+            </div>
+            <div class="contact-right">
+              <p>(647) 712 6119</p>
+            </div>
+          </div>
+          <div class="contact">
+            <div class="contact-left">
+              <i class="far fa-envelope"></i>
+            </div>
+            <div class="contact-right">
+              <p>contact@blueflowermedia.com</p>
+            </div>
+          </div>
+
+          <div class="contact">
+            <div class="contact-left">
+              <i class="fas fa-map-marker-alt"></i>
+            </div>
+            <div class="contact-right">
+              <p>3214 Danforth Ave<br>Toronto, Ontario, Canada</p>
+            </div>
+          </div>
+
+          <div class="contact">
+            <div class="contact-left">
+              <i class="far fa-clock"></i>
+            </div>
+            <div class="contact-right">
+              <p>MON - FRI: 10:00 AM to 6:00 PM EST</p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <!-- <div class="contact-map flex2">
@@ -29,7 +69,7 @@
       </div> -->
       <div class="contact-form flex3" >
         <!-- <h2>Get in touch!</h2> -->
-        <?php echo do_shortcode( '[contact-form-7 id="42" title="Contact Form"]' ) ?>
+        <?php echo do_shortcode( '[contact-form-7 id="42" title="Contact Form" ]' ) ?>
       </div>
     </div>
   </div>
@@ -40,12 +80,7 @@
     <iframe src="https://snazzymaps.com/embed/76304" width="100%" height="500px" style="border:none;"></iframe>
   </div>
 </section>
-<!-- <section id="contact-form">
-  <div class="container">
-    <h2>Get in touch!</h2>
-    <?php echo do_shortcode( '[contact-form-7 id="54" title="Contact Form 1"]' ) ?>
-  </div>
-</section> -->
+
 
 
 <!-- section-getintouch ---------------------------------------------------->
