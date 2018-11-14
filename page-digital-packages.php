4b30eb212bb58657e11ccadb0d66b1b881e39c2c
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-14 17:21:27 -0500

    images changed

--- page-digital-packages.php
+++ page-digital-packages.php
@@ -7,9 +7,9 @@
     </div>
     <div class="txtservice flex1">
       <h1>Digital Packages<span class="big-dot blue">.</span></h1>
-      <p>That's why our websites don't only look great, they also drive results. To help achieve your core business goals, we developed
-         a holistic web design process that brings together design, technology, content, and marketing to create the best web experience.
-       </p>
+      <p>
+        Packages help simplify pricing for businesses. We created our packages based on services that we receive the most. Packages can be customized, with options added or removed based on what you desire!
+      </p>
     </div>
   </div>
 </section>
@@ -20,7 +20,7 @@
     <div class="package-left flex1">
       <p><span class="yellow-background">Most Popular</span></p>
       <h3>New Business Package</h3>
-      <p>Lorem ipsum dolor sit amet,Nunc Phasellus lobortis augue libero, id euismod enim pellentesque et. Nunc nec arcu vitae tellus lobortis hendrerit. Sed luctus purus turpis, et porttitor lacus accumsan id.</p>
+      <p>Are your just starting your business? Looking to save money and time? We’ve got the perfect package for you! Our New Business Package provides small business owners the basics for your branding, online presence along with business cards.</p>
       <img class="shown-in-mobile-only" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset11.svg" alt="" class="image">
 
       <div class="accordion1">
@@ -28,11 +28,11 @@
       <div class="panel">
         <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
         <ul>
-          <li>1 hour Website Consultation</li>
-          <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-          <li>Custom Wordpress Theme</li>
-          <li>Responsive & Cross Browser Tested</li>
-          <li>Google Console Setup + Google Analytics Setup</li>
+          <li>1 hour discovery/brainstorming session</li>
+          <li>3 Logo Variations</li>
+          <li>5 Rounds of revisions for chosen logo variation</li>
+          <li>Logo files based on required usage (eg; business cards, favicon, website, merch, etc.)</li>
+          <li>All Source Files</li>
         </ul>
 
       </div>
@@ -41,11 +41,17 @@
       <div class="panel">
         <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
         <ul>
-          <li>1 hour Website Consultation</li>
-          <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-          <li>Custom Wordpress Theme</li>
-          <li>Responsive & Cross Browser Tested</li>
-          <li>Google Console Setup + Google Analytics Setup</li>
+          <li>1 hour consultation and strategizing (in-person/Video Conference)</li>
+          <li>Domain Setup / 1 Year Domain</li>
+          <li>1 Year Hosting</li>
+          <li>1 Year SSL Certificate + WhoIs Protection</li>
+          <li>Wordpress Theme Customization</li>
+          <li>5 Website Sections (e.g; Home, About, Services, Gallery, Contact)</li>
+          <li>1 Contact form + 1 newsletter form</li>
+          <li>Social Media Integration</li>
+          <li>Basic Google Console Setup + Google Analytics Setup</li>
+          <li>Basic SEO optimization (meta tags) + XML Sitemap</li>
+          <li>1 hour website coaching</li>
         </ul>
       </div>
 
@@ -53,11 +59,10 @@
       <div class="panel">
         <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
         <ul>
-          <li>1 hour Website Consultation</li>
-          <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-          <li>Custom Wordpress Theme</li>
-          <li>Responsive & Cross Browser Tested</li>
-          <li>Google Console Setup + Google Analytics Setup</li>
+          <li>Business Card Design based on chosen logo</li>
+          <li>2500 Glossy 100DPI Full Color - Double Sided</li>
+          <li>2 Rounds of Revisions</li>
+          <li>Source Files</li>
         </ul>
       </div>
       </div>
@@ -87,17 +92,6 @@
         <div class="package-content">
           <p>$ 750.00</p>
           <ul class="package-list">
-            <!-- <li>1 hour Website Consultation</li>
-            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-            <li>Custom Wordpress Theme</li>
-            <li>Responsive and Cross Browser Tested</li>
-            <li>Google Console Setup + Google Analytics Setup</li>
-            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
-            <li>1 Hour Website Coaching</li>
-            <li>Social Media Integration</li>
-            <li>1 Contact Form + Newsletter Form</li>
-            <li>5 Website Sections(e.g., Home,About, Services, Gallery, Contact)</li> -->
-
             <li>1 hour consultation and strategizing (in-person/Video Conference)</li>
             <li>Domain Setup / 1 Year Domain</li>
             <li>1 Year Hosting</li>
@@ -137,9 +131,6 @@
             <li>Search Console Setup + Google Analytics Setup</li>
             <li>Basic SEO optimization (meta tags) + XML Sitemap</li>
             <li>1 hour website coaching</li>
-
-            <!-- <li><strong>Extra Features</strong></li> -->
-
           </ul>
         </div>
       </div>
@@ -155,99 +146,96 @@
         <div class="package-content">
           <p>$2,800.00</p>
           <ul class="package-list">
-            <li>1 hour Website Consultation</li>
-            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-            <li>Custom Wordpress Theme</li>
-            <li>Responsive and Cross Browser Tested</li>
-            <li>Google Console Setup + Google Analytics Setup</li>
-            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
-            <li>1 Hour Website Coaching</li>
-            <li>Social Media Integration</li>
-            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
-            <li>5 Website Pages</li>
-            <li>3 Sections Per Page</li>
-            <li>5 Custom Icons</li>
+            <li>Wordpress Theme Customization</li>
+            <li>Pages</li>
+
+              <li class="margin-left">Home</li>
+              <li class="margin-left">About</li>
+              <li class="margin-left">Product listing by categories</li>
+              <li class="margin-left">Single Product</li>
+              <li class="margin-left">Product Search Results</li>
+              <li class="margin-left">Cart, Payment, Wish list</li>
+              <li class="margin-left">Policies</li>
+              <li class="margin-left">User Registration and Login</li>
+
+            <li>Product rating and comments</li>
+            <li>Filtering products by upto 3 properties</li>
+            <li>CMS installation (WooCommerce/Shopify)</li>
+            <li>Payment gateway installation (Paypal/Stripe)</li>
+            <li>Ecommerce Google Analytics Basic</li>
+            <li>Conversion Strategies</li>
+              <li class="margin-left">Automated new customer offers</li>
+              <li class="margin-left">Newsletter signup</li>
+              <li class="margin-left">Live Chat</li>
+
           </ul>
         </div>
       </div>
 
-      <!-- service 4 --->
+
+            <!-- service 4 --->
+            <div class="package">
+              <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
+                <div class="logo">
+                  <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/webdesign/gradient/Server.svg" alt="bfm-digital-marketing-icon">
+                  <h3>Website Maintenance</h3>
+                </div>
+              </a>
+              <div class="package-content">
+                <p>$300.00/Month</p>
+                <ul class="package-list">
+
+                  <li>Written Content Management: Add, Edit, Remove</li>
+                  <li>Photo Management: Add, Edit, Remove, Photoshop</li>
+                  <li>Video Management: Add, Edit, Remove</li>
+                  <li>Navigation Changes: Add, Edit, Move, Remove</li>
+                  <li>Pages: Add, Edit, Remove</li>
+                  <li>Forms: Creating, Customizing, Testing</li>
+                  <li>Daily Malware Scans & Website Backup</li>
+                  <li>Dedicated 5 hours/Month</li>
+
+                </ul>
+              </div>
+            </div>
+      <!-- service 5 --->
       <div class="package">
         <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
           <div class="logo">
-            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset9.svg" alt="bfm-digital-marketing-icon">
+            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/graphic-design/gradient/BrandStyleGuide.svg" alt="bfm-digital-marketing-icon">
             <h3>Brand Style Guide</h3>
           </div>
         </a>
         <div class="package-content">
-          <p>$2,800.00</p>
+          <p>$1,200.00</p>
           <ul class="package-list">
-            <li>1 hour Website Consultation</li>
-            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-            <li>Custom Wordpress Theme</li>
-            <li>Responsive and Cross Browser Tested</li>
-            <li>Google Console Setup + Google Analytics Setup</li>
-            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
-            <li>1 Hour Website Coaching</li>
-            <li>Social Media Integration</li>
-            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
-            <li>5 Website Pages</li>
-            <li>3 Sections Per Page</li>
-            <li>5 Custom Icons</li>
+            <li>Standard logo +</li>
+            <li>Color Guidelines</li>
+            <li>Typography Hierarchy Guidelines</li>
+            <li>Imaging Guidelines</li>
+            <li>5 Custom Icons + Iconography Guidelines</li>
+            <li>Brand Elements Guidelines</li>
+            <li>1 Hour Brand Consistency Coaching</li>
           </ul>
         </div>
       </div>
 
-      <!-- service 4 --->
-      <div class="package">
-        <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
-          <div class="logo">
-            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset9.svg" alt="bfm-digital-marketing-icon">
-            <h3>Website Maintenance</h3>
-          </div>
-        </a>
-        <div class="package-content">
-          <p>$2,800.00</p>
-          <ul class="package-list">
-            <li>1 hour Website Consultation</li>
-            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-            <li>Custom Wordpress Theme</li>
-            <li>Responsive and Cross Browser Tested</li>
-            <li>Google Console Setup + Google Analytics Setup</li>
-            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
-            <li>1 Hour Website Coaching</li>
-            <li>Social Media Integration</li>
-            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
-            <li>5 Website Pages</li>
-            <li>3 Sections Per Page</li>
-            <li>5 Custom Icons</li>
-          </ul>
-        </div>
-      </div>
 
-      <!-- service 4 --->
+      <!-- service 6 --->
       <div class="package">
         <a class="service-anchor" href="<?php echo $digitalmarketing; ?>">
           <div class="logo">
-            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/digital-packages/Asset9.svg" alt="bfm-digital-marketing-icon">
+            <img class="serv-logo" src="<?php echo get_template_directory_uri(); ?>/images/graphic-design/gradient/Logo.svg" alt="bfm-digital-marketing-icon">
             <h3>Logo Design</h3>
           </div>
         </a>
         <div class="package-content">
-          <p>$2,800.00</p>
+          <p>$450.00</p>
           <ul class="package-list">
-            <li>1 hour Website Consultation</li>
-            <li>1 Year Domain, Hosting, SSL, Whols Protection</li>
-            <li>Custom Wordpress Theme</li>
-            <li>Responsive and Cross Browser Tested</li>
-            <li>Google Console Setup + Google Analytics Setup</li>
-            <li>Basic SEO (meta tags) + (XML Sitemap)</li>
-            <li>1 Hour Website Coaching</li>
-            <li>Social Media Integration</li>
-            <li>Contact Form, Newsletter Form, Registration Form, Custom Form</li>
-            <li>5 Website Pages</li>
-            <li>3 Sections Per Page</li>
-            <li>5 Custom Icons</li>
+            <li>1 hour discovery/brainstorming session</li>
+            <li>3 Logo Variations</li>
+            <li>5 Rounds of revisions for chosen logo variation</li>
+            <li>Logo files based on required usage (eg; business cards, favicon, website, merch, etc.)</li>
+            <li>All Source Files</li>
           </ul>
         </div>
       </div>
