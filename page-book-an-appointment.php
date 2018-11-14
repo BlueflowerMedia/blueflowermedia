8dce091df0338d2b48f2bd0bbe9a4bac48ed462e
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-14 14:53:25 -0500

    calendly page height

--- page-book-an-appointment.php
+++ page-book-an-appointment.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="calendly-inline-widget" data-url="https://calendly.com/blueflowermedia/consultation" style="min-width:320px;height:auto;margin-top:15vh"></div>
+<div class="calendly-inline-widget" data-url="https://calendly.com/blueflowermedia/consultation" style="min-width:320px;height:800px;margin-top:15vh"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
 <!-- section-getintouch ---------------------------------------------------->
 <?php get_template_part('template-parts/getintouch'); ?>
