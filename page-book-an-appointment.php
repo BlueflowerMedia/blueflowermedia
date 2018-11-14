2db0a6f60a551658532bdfc07afb2d59dd862ee2
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-14 14:39:38 -0500

    book an appointment page changed

--- page-book-an-appointment.php
+++ page-book-an-appointment.php
@@ -2,5 +2,6 @@
 
 <div class="calendly-inline-widget" data-url="https://calendly.com/blueflowermedia/consultation" style="min-width:320px;height:580px;margin-top:15vh"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
-
+<!-- section-getintouch ---------------------------------------------------->
+<?php get_template_part('template-parts/getintouch'); ?>
 <?php get_footer(); ?>
