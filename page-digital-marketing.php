b408edc906f6f284aef402ab4ec4243981685777
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-15 13:42:16 -0500

    digital marketing page content changes

--- page-digital-marketing.php
+++ page-digital-marketing.php
@@ -7,7 +7,7 @@
     </div>
     <div class="txtservice flex3">
       <h1>Digital Marketing<span class="big-dot blue">.</span></h1>
-      <p>In a multi-device world, its very important to attract and retain users on as many touch points as possible. For us, the first step is to have a great presence on as many relevant platforms as possible in a way that makes the most of each platforms. </p>
+      <p>In a multi-device world, its very important to attract and retain users on as many touch points as possible. For us, the first step is to have a great presence on as many relevant platforms as possible in a way that makes the most of each platforms. It truly is a requirement to have a user experience that converts on as many relevant platforms as possible.  The need for amazing design will be very obvious here in this stage of your digital marketing process, as great design just converts so much better than bland design! Now begins your digital marketing journey. </p>
     </div>
   </div>
 </section>
@@ -31,10 +31,12 @@
     <div class="text-left">
       <h3><span class="yellow-text">3.5 billion </span>people online.</h3>
       <h3>Countless marketing options.</h3>
-      <h3>What now?</h3>
+      <h3>What to do now?</h3>
     </div>
     <div class="text-right">
-      <p>In a multi-device world, its very important to attract and retain users on as many touch points as In a multi-device world, its very important to attract and retain users on as many touch points as </p>
+      <p>Small business owners can have a hard time navigating through the clutter of the digital marketing world. With so many seemingly attractive options, it’s important for business owners to have a strategy that is focused with deliberate achievable milestones. </p>
+      <p><strong>Not sure how you want to market your business online?</strong></p>
+      <p>That’s why we’re here! Understanding your industry, your business model, and your current target audience will help us in creating a marketing plan for you. We analyze what your competitors are doing & what your audience is searching online & then create a growth plan accordingly. </p>
       <hr>
     </div>
   </div>
@@ -106,7 +108,7 @@
       <h3>Your business is <span class="yellow-text">unique</span>.<br> Your marketing strategy <br>should be the same.</h3>
     </div>
     <div class="text-right">
-      <p>In a multi-device world, its very important to attract and retain users on as many touch points as In a multi-device world, its very important to attract and retain users on as many touch points as </p>
+      <p>Most bland prepackaged marketing strategies fail. There are plenty of common marketing tactics used by businesses that perform very well - but the questions is: are those marketing tactics right for you? The right marketing plan for you is one that truly understands your business and your customers and aims to create a connection in the most organic way possible. All of marketing plans are different for each client. </p>
       <hr>
     </div>
   </div>
@@ -125,16 +127,12 @@
         <div class="text">
           <div class="text-top">
             <h3>Search Engine Optimization</h3>
-            <p>Small business owners can have a hard time
-              navigating through the clutter of the digital
-              marketing world.Small business owners can have a hard time
-              navigating through the clutter of the digital
-              marketing world.Small business owners can have a hard time
-              navigating through the clutter of the digital
-              marketing world.</p>
+            <p>Increase your website ranking organically with the power of Search Engine Optimization! Search engines do not understand web pages the same way humans do, making it apparent why it’s important to make your website search engine friendly. Optimizing your website for search engines allows them to put you on the top for keywords you want to rank for.</p>
           </div>
           <div class="text-bottom">
-            <p>Sub Services: Google Analytics Integration, Search Console Setup, Yoast Plugin Setup, Keyword Research & Strategy, Competitor Analysis, Content Writing, Backlink Management</p>
+            <p>Sub Services: Google Analytics Integration, Search Console Setup, Keyword Research & Strategy, Competitor Analysis, Content Writing, Backlink Management, Speed Optimization,
+Social Media Management
+</p>
           </div>
 
         </div>
@@ -146,16 +144,10 @@
         <div class="text">
           <div class="text-top">
             <h3>Social Media Management</h3>
-            <p>Small business owners can have a hard time
-              navigating through the clutter of the digital
-              marketing world.Small business owners can have a hard time
-              navigating through the clutter of the digital
-              marketing world.Small business owners can have a hard time
-              navigating through the clutter of the digital
-              marketing world.</p>
+            <p>Social Media Management allows small business owners to stay engage with their followers on the platforms they use the most! With over 1 Billion people using social media, you open your business to huge audience. Update them on your products/services, create posts that promote your brand values, create engagement opportunities with polls & giveaways, advertise and re-target based on your potential customers interests, and much more. </p>
           </div>
           <div class="text-bottom">
-            <p>Sub Services: Google Analytics Integration, Search Console Setup, Yoast Plugin Setup, Keyword Research & Strategy, Competitor Analysis, Content Writing, Backlink Management</p>
+            <p>Sub Services: Social Media Account Creation, Social Media Account Optimization, Graphic Design, Content Writing, Social Media Posting Automation, Platform Verification</p>
           </div>
 
         </div>
@@ -167,16 +159,10 @@
         <div class="text">
           <div class="text-top">
             <h3>Digital Advertising</h3>
-             <p>Small business owners can have a hard time
-                navigating through the clutter of the digital
-                marketing world.Small business owners can have a hard time
-                navigating through the clutter of the digital
-                marketing world.Small business owners can have a hard time
-                navigating through the clutter of the digital
-                marketing world.</p>
+            <p>Boost your business brand awareness and sales as soon as possible with PPC advertising. Allow people looking for your products/services to find you right away! PPC advertising will show you results the quickest, and with the right campaigns, can bring you incredible ROI (Return On Investment). Advertise on search engines, media/publishing companies, social platforms, etc. </p>
           </div>
           <div class="text-bottom">
-            <p>Sub Services: Google Analytics Integration, Search Console Setup, Yoast Plugin Setup, Keyword Research & Strategy, Competitor Analysis, Content Writing, Backlink Management</p>
+            <p>Sub Services: Search Campaign Management, Display Campaign Management, Video Campaign Management, Shopping Campaign Management, App Promotion Campaign Management, Google Advertising, Bing Advertising, Social Advertising</p>
           </div>
 
         </div>
