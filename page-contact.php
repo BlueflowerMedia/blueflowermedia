98745e8f227bb991a504a2b13225c38d7b16a1e7
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-15 13:24:59 -0500

    more changes in contact page

--- page-contact.php
+++ page-contact.php
@@ -4,29 +4,12 @@
 <div class="container no-bottom-padding">
   <h1>Contact Us<span class="big-dot blue">.</span></h1>
 </div>
+
+
 <section id="section-contact">
-  <div class="container">
-
-    <div class="row flex-row">
-      <!-- <div class="contact-type flex1">
-        <i class="fas fa-mobile-alt"></i>
-        <h4>Phone Number</h4>
-        <p>647-712-6119</p>
-        <i class="fas fa-envelope-open"></i>
-        <h4>E-mail</h4>
-        <p>contact@blueflowermedia.com</p>
-        <i class="fas fa-map-marker-alt"></i>
-        <h4>Location</h4>
-        <p>3214 Danforth Ave,<br>Toronto, ON M1L 1C1</p>
-        <i class="fas fa-clock"></i>
-        <h4>Hours</h4>
-        <p>10 AM to 6 PM</p>
-
-      </div> -->
-
-      <div class="footer-right flex1">
-
-        <div class="contact-items">
+  <div class="contact-container flex-row">
+    <div class="contact-details-container">
+        <div class="contact-items flex1">
           <div class="contact">
             <div class="contact-left">
               <i class="fas fa-mobile-alt"></i>
@@ -62,14 +45,13 @@
             </div>
           </div>
         </div>
-      </div>
+    </div>
 
-      <!-- <div class="contact-map flex2">
-        <iframe src="https://snazzymaps.com/embed/76304" width="100%" height="500px" style="border:none;"></iframe>
-      </div> -->
-      <div class="contact-form flex3" >
+
+    <div class="contact-form-container">
+      <div class="contact-form flex1" >
         <!-- <h2>Get in touch!</h2> -->
-        <?php echo do_shortcode( '[contact-form-7 id="42" title="Contact Form"]' ) ?>
+        <?php echo do_shortcode( '[contact-form-7 id="42" title="Contact Form "]' ) ?>
       </div>
     </div>
   </div>
