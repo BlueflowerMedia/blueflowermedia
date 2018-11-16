a3fdebe5e9c6f74988957a42da7b1fe56a0276f0
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-16 17:37:46 -0500

    third row of the our work page changed

--- page-our-work.php
+++ page-our-work.php
@@ -81,64 +81,6 @@
       </div>
     </div>
 
-
-    <!-- 3rd row -->
-    <div class="item-row">
-      <!-- 1st -->
-      <div class="item">
-        <div class="css-slideshow">
-      	<figure>
-      		<img src="<?php echo get_template_directory_uri(); ?>/images/Portfolio Photos/india-journal-portfolio-photo-1.png" alt="class-header-css3" width="495" height="370" class="alignnone size-full wp-image-172" /><figcaption></figcaption>
-      	</figure>
-      	<figure>
-      		<img src="http://themarklee.com/wp-content/uploads/2013/10/class-header-semantics.jpg" alt="class-header-semantics" width="495" height="370" class="alignnone size-full wp-image-179" /><figcaption></figcaption>
-      	</figure>
-      	<figure>
-      		<img src="http://themarklee.com/wp-content/uploads/2013/10/class-header-offline.jpg" alt="class-header-offline" width="495" height="370" class="alignnone size-large wp-image-178" /><figcaption></figcaption>
-      	</figure>
-      	<figure>
-      		<img src="http://themarklee.com/wp-content/uploads/2013/10/class-header-device.jpg" alt="class-header-device" width="495" height="370" class="alignnone size-full wp-image-177" /><figcaption><strong>Device Access:</strong> Beginning with the Geolocation API, Web Applications can present rich, device-aware features and experiences. Incredible device access innovations are being developed and implemented, from audio/video input access to microphones and cameras, to local data such as contacts &amp; events, and even tilt orientation.</figcaption>
-      	</figure>
-        </div>
-
-        <h4>Master Pizzeria</h4>
-        <div class="project-tags">
-          <button class="tag web" disabled>Web design</button>
-          <button class="tag graphics" disabled>Plugin Development</button>
-          <button class="tag web" disabled>Graphic Design</button>
-        </div>
-      </div>
-      <!-- 2nd -->
-      <div class="item">
-        <a href="<?php echo get_permalink(get_page_by_path( 'portfolio-mobilekare' )); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/Portfolio Photos/mobile-kare-portfolio-photo-1.png" alt="" class="image"></a>
-        <h4>Mobicare</h4>
-        <div class="project-tags">
-          <!-- <button class="tag web" disabled>Web Desisergn</button> -->
-          <button class="tag web" disabled>Logo Design</button>
-          <button class="tag graphics" disabled>E-Commerce Design</button>
-          <button class="tag web" disabled>Product/Inventory Management</button>
-          <button class="tag graphics" disabled>Multi Platform Product Upload Automation</button>
-          <button class="tag web" disabled>Web/Print Design</button>
-        </div>
-      </div>
-
-      <!-- 3rd -->
-      <div class="item">
-        <a href="<?php echo get_permalink(get_page_by_path( 'portfolio-mobilekare' )); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/Portfolio Photos/mobile-kare-portfolio-photo-1.png" alt="" class="image"></a>
-        <h4>Mobicare</h4>
-        <div class="project-tags">
-          <!-- <button class="tag web" disabled>Web Desisergn</button> -->
-          <button class="tag web" disabled>Logo Design</button>
-          <button class="tag graphics" disabled>E-Commerce Design</button>
-          <button class="tag web" disabled>Product/Inventory Management</button>
-          <button class="tag graphics" disabled>Multi Platform Product Upload Automation</button>
-          <button class="tag web" disabled>Web/Print Design</button>
-        </div>
-      </div>
-    </div>
-
-
-
   </div>
 
 </section>
