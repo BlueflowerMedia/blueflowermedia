ec4237413b5399f6e6a4efcb802fba6c4e104846
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-23 16:53:41 -0500

    careers page added

---
+++ page-careers.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+
+<section id="careers">
+  <div class="container">
+    <div class="txtservice flex1">
+      <h3>Blueflower Media Corp. Careers<span class="big-dot blue">.</span></h3>
+    </div>
+    <br><br><br>
+    <a href="<?php echo get_permalink(get_page_by_path( 'careers-lead-designer-remote' )); ?>">
+      <h3>Lead Designer</h3>
+      <h4>Location: Remote/Phillipines</h4>
+    </a>
+    <br><br><br>
+    <a href="<?php echo get_permalink(get_page_by_path( 'careers-lead-developer-remote' )); ?>">
+      <h3>Lead Developer</h3>
+      <h4>Location: Remote/Phillipines</h4>
+    </a>
+
+  </div>
+
+</section>
+
+
+<?php get_footer(); ?>
