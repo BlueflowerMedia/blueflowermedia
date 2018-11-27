ca7996e33ed4dcb9dc4a4cbb49412c0eb3803db5
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-27 14:50:08 -0500

    some more changes in the contact form

--- page-contact.php
+++ page-contact.php
@@ -51,7 +51,7 @@
     <div class="contact-form-container">
       <div class="contact-form flex1" >
         <!-- <h2>Get in touch!</h2> -->
-        <?php echo do_shortcode('[contact-form-7 id="142" title="packages"]' ) ?>
+        <?php echo do_shortcode( '[contact-form-7 id="42" title="Contact form"]' ) ?>
       </div>
     </div>
   </div>
