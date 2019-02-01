933bde261e011b1d52d665eb1f6bfbd6a71b89ac
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2019-02-01 14:21:44 -0500

    new blog page design

--- page-blog.php
+++ page-blog.php
@@ -3,29 +3,29 @@
 <?php $query = new WP_Query(" "); ?>
 <section id="blog">
   <div class="container flex-column">
-    <h1>Our Blog<span class="big-dot blue">.</span></h1>
-    <p>We talk about our services, what goes on at work & what we learn along the way.</p>
+    <h1>Blog<span class="big-dot blue">.</span></h1>
   </div>
 </section>
 
 <section>
-  <div class="container">
+  <div class="container5">
     <div class="posts">
       <?php if ( $query->have_posts() ): while ( $query->have_posts() ): $query->the_post(); ?>
         <div class="post">
           <div class="post-image">
             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-            <div class="post-info">
-              <a href="<?php the_permalink(); ?>" title="Read"><h2><?php the_title(); ?></h2></a>
-              <p class="date"><?php echo  get_the_time('M j, Y'); ?></p>
-              <?php the_excerpt(); ?>
-              <a href="<?php echo get_permalink(); ?>" class="read-more"> Read More
-                <span class="arrow">
-                  <!-- <svg height="15px" width="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"/></svg> -->
-                  <i class="fas fa-angle-double-right"></i>
-                </span>
-              </a>
-            </div>
+
+          </div>
+          <div class="post-info">
+            <a href="<?php the_permalink(); ?>" title="Read"><h2><?php the_title(); ?></h2></a>
+            <p class="date"><?php echo  get_the_time('M j, Y'); ?></p>
+            <?php the_excerpt(); ?>
+            <a href="<?php echo get_permalink(); ?>" class="read-more"> Read More
+              <span class="arrow">
+                <!-- <svg height="15px" width="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"/></svg> -->
+                <i class="fas fa-angle-double-right"></i>
+              </span>
+            </a>
           </div>
         </div>
       <?php endwhile; else: ?>
