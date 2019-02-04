819be6000ed0a10ff0d511f40c7c633f4bd31749
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2019-02-04 13:37:04 -0500

    gif added back

--- front-page.php
+++ front-page.php
@@ -67,9 +67,7 @@ $cloudhosting = get_permalink(get_page_by_path( 'cloud-hosting' ));
 <section id="section-aboutus">
   <div class="container2 flex-row">
     <div class="container-left">
-      <div id="lottie-id">
-        <p>hello</p>
-      </div>
+      <img class="mySlides6" src="<?php echo get_template_directory_uri(); ?>/images/animations/bfm-animation.gif" alt="" >
     </div>
     <div class="container-right">
       <h1 class="black">About Us<span class="big-dot black">.</span></h1>
